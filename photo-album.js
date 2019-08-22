92d0ed2ce8ecbefd57ab51beca7b5c2822ac6a76
Ben Trettin <batrettin@gmail.com>
2019-08-22 18:32:28 -0500

    Added testing dependencies and skeleton code

---
+++ photo-album.js
@@ -0,0 +1,3 @@
+const photoAlbum = () => {};
+
+module.exports = photoAlbum;
---
+++ test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const app = require('./photo-album.js');
+
+describe('dummy function', () => {
+  it('should do something', () => {
+    expect(5).to.equal(3 + 2);
+  });
+});
