9033634ceda1d9959a44429542d2a4a6ed059405
Ben Trettin <batrettin@gmail.com>
2019-08-22 18:53:18 -0500

    added get request and logged each object in the body to pass the first test.

--- src/photo-album.js
+++ src/photo-album.js
@@ -1,3 +1,10 @@
-const photoAlbum = () => {};
+const request = require('request');
+
+const photoAlbum = () => {
+  request.get(
+    'https://jsonplaceholder.typicode.com/photos?albumId=' + process.argv[2],
+    (err, res, body) => JSON.parse(body).forEach(photo => console.log(photo))
+  );
+};
 
 module.exports = photoAlbum;
