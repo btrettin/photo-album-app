aab0313c103709ac9ac5132d19d057c644e9e261
Ben Trettin <batrettin@gmail.com>
2019-08-22 20:06:32 -0500

    moved code out into consts

--- src/photo-album.js
+++ src/photo-album.js
@@ -1,20 +1,21 @@
 const request = require('request');
 
+const arg = process.argv[2];
+const url = 'https://jsonplaceholder.typicode.com/photos?albumId=' + arg;
 const errMsg = 'Argument must be an int from 1-100. Ex: node photo-album 43';
 
 const isInt = str => /^\d+$/.test(str);
 
-const photoAlbum = id => {
-  isInt(id) && id > 0 && id < 101
-    ? request.get(
-        'https://jsonplaceholder.typicode.com/photos?albumId=' +
-          process.argv[2],
-        (err, res, body) =>
-          JSON.parse(body).forEach(photo =>
-            console.log(`[${photo.id}] ${photo.title}`)
-          )
+const photoAlbum = albumId => {
+  isInt(albumId) && albumId > 0 && albumId < 101
+    ? request.get(url, (err, res, body) =>
+        JSON.parse(body).forEach(photo =>
+          console.log(`[${photo.id}] ${photo.title}`)
+        )
       )
     : console.log(errMsg);
 };
 
-module.exports = { photoAlbum, isInt };
+photoAlbum(arg);
+
+module.exports = { isInt, photoAlbum };
