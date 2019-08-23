8ad1f7508b779c909c4f9c16c98bc42ac9f78780
Ben Trettin <batrettin@gmail.com>
2019-08-22 19:32:57 -0500

    added functionality for checking that id is between 1-100 and printing error if not

--- src/photo-album.js
+++ src/photo-album.js
@@ -1,13 +1,16 @@
 const request = require('request');
 
-const photoAlbum = () => {
-  request.get(
-    'https://jsonplaceholder.typicode.com/photos?albumId=' + process.argv[2],
-    (err, res, body) =>
-      JSON.parse(body).forEach(photo =>
-        console.log(`[${photo.id}] ${photo.title}`)
+const photoAlbum = id => {
+  id < 100 && id > 0
+    ? request.get(
+        'https://jsonplaceholder.typicode.com/photos?albumId=' +
+          process.argv[2],
+        (err, res, body) =>
+          JSON.parse(body).forEach(photo =>
+            console.log(`[${photo.id}] ${photo.title}`)
+          )
       )
-  );
+    : console.log('invalid argument');
 };
 
 module.exports = photoAlbum;
