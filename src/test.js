223b7b16fa12f2814582cc4dd1dc5e7a7269af89
Ben Trettin <batrettin@gmail.com>
2019-08-22 19:02:55 -0500

    added tests for checking specific log output as well as checking if json parser is called

--- src/test.js
+++ src/test.js
@@ -5,11 +5,32 @@ const request = require('request');
 const body = require('./test-body.js');
 
 describe('photoAlbum', () => {
+  beforeEach(() => {
+    sinon.stub(request, 'get').yields(null, null, body);
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('should parse the body of an http request', done => {
+    sinon.spy(JSON, 'parse');
+    app.photoAlbum('4');
+    expect(JSON.parse.callCount).to.equal(1);
+    done();
+  });
   it('should log 50 times', done => {
     sinon.spy(console, 'log');
-    sinon.stub(request, 'get').yields(null, null, body);
-    app('4');
+    app.photoAlbum('4');
     expect(console.log.callCount).to.equal(50);
+    done();
+  });
+  it('should provide the correct arguments to the first log', done => {
+    sinon.spy(console, 'log');
+    app.photoAlbum('4');
+    expect(
+      console.log.firstCall.calledWith(
+        '[151] possimus dolor minima provident ipsam'
+      )
+    ).to.be.true;
     done();
   });
 });
